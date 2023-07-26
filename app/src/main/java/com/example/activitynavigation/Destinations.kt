41bb0b8a7aecb0ce429f2ddf1e0ed138ccd9f1fa
Rasel Rahman <rasel.cse19.ruet@gmail.com>
2023-07-27 01:44:52 +0600

    Tabbed/Bottom Navigation added

--- app/src/main/java/com/example/activitynavigation/Destinations.kt
+++ app/src/main/java/com/example/activitynavigation/Destinations.kt
@@ -1,5 +1,10 @@
 package com.example.activitynavigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
 interface Destinations{
     val route:String
 }
@@ -16,4 +21,20 @@ object DashBoard:Destinations{
 }
 object Details:Destinations{
     override val route: String = "Details"
+}
+//This interface is for Bottom navigation
+interface Destination{
+    val route : String
+    val icon : ImageVector
+    val title : String
+}
+object HomeB : Destination{
+    override val route = "HommeB"
+    override val icon = Icons.Filled.Home
+    override  val title = "Home"
+}
+object SettingB : Destination{
+    override val route = "SettingB"
+    override val icon = Icons.Filled.Settings
+    override  val title = "Setting"
 }
--- app/src/main/java/com/example/activitynavigation/MainActivity.kt
+++ app/src/main/java/com/example/activitynavigation/MainActivity.kt
@@ -1,26 +1,88 @@
 package com.example.activitynavigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MenuInflater
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.activitynavigation.HomeB.icon
 import java.lang.reflect.Type
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyNavigation()
+            //MyNavigation()
+            MyApp()
         }
     }
 }
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(){
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            MyBottomNavigation(navController = navController)
+        }
+    ) {
+        Box(modifier = Modifier.padding(it) ){
+            NavHost(navController = navController, startDestination = HomeB.route ){
+                composable(HomeB.route){
+                    HomeBScreen()
+                }
+                composable(SettingB.route){
+                    SettingsScreen()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun MyBottomNavigation(navController: NavController){
+    val destinationList = listOf<Destination>(
+        HomeB,SettingB
+    )
+    val selectedIndex  = rememberSaveable {
+        mutableStateOf(0)
+    }
+    BottomNavigation() {
+        destinationList.forEachIndexed(){index, destination ->
+            BottomNavigationItem(
+                label = { Text(text = destination.title)},
+                icon = { Icon(imageVector = destination.icon, contentDescription = destination.title )},
+                selected = selectedIndex.value == index,
+                onClick = {
+                    selectedIndex.value = index
+                    navController.navigate(destinationList[index].route){
+                        popUpTo(HomeB.route)
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
+}
 @Composable
 fun MyNavigation() {
     val navController = rememberNavController()
